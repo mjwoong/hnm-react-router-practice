1b6657fdc538d59448ea92109ed82fbd9597799a
문재웅 <answodnd112@gmail.com>
2023-04-21 17:46:04 +0900

    router setting

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { Routes, Route } from 'react-router';
 import "./App.css";
+import ProductAll from './page/ProductAll';
+import Login from './page/Login';
+import ProductDetail from './page/ProductDetail';
+
+// 1. 전체 상품 페이지, 로그인 페이지, 상품 상세 페이지
+// 2. 전체 상품 페이지에서는 전체 상품 페이지를 볼 수 있다.
+// 3. 로그인 버튼을 누르면 로그인 페이지가 나온다.
+// 3. 상품 디테일을 눌렀으나 비로그인 상태에서는 로그인 페이지로 이동
+// 4. 로그인이 된 상태에서는 상품 디테일 페이지를 볼 수 있다.
+// 5. 로그아웃 버튼을 클릭하면 로그아웃이 된다.
+// 5. 로그아웃이 되면 상품 디테일 페이지를 볼 수 없다. 다시 로그인 페이지가 보인다.
+// 6. 로그인을 하면 로그아웃이 보이고, 로그아웃을 하면 로그인이 보인다.
+// 7. 상품을 검색할 수 있다.
 
 function App() {
-  return <div></div>;
+  return <div>
+    <Routes>
+      <Route path="/" element={<ProductAll/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/product/:id" element={<ProductDetail/>}/>
+    </Routes>
+  </div>;
 }
 
 export default App;
